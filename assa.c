f9d3dfffbe1295f6a4e9402a13326ba287319702
Stefan Rajinovic <rajinovic@student.tugraz.at>
2016-12-06 13:09:42 +0100

    Prototip funkija

--- assa.c
+++ assa.c
@@ -26,6 +26,149 @@ typedef struct _Person_
   struct _Person_ *father_; 
 }Person;
 
+
+Person *parseDotFile(char *file_content);
+
+BOOL isValidName(char *name);
+
+void parseInput(char *input_command, Person *persons_array);
+
+void waitForInput(Person *persons_array);
+
+BOOL fileExists(const char *file_name);
+
+BOOL fileIsWritable(const char *file_name);
+
+char *storeFileIntoMemory(const char *file_name);
+
+void listPersons(Person *persons);
+
+Person *createPersonInstance(char *name, BOOL gender, Person *mother, Person *father);
+
+Person *findPerson(Person *persons, int number_of_persons, char *name, BOOL gender);
+
+/**
+ * [main description]
+ * @param  argc [description]
+ * @param  argv [description]
+ * @return      [description]
+ */
+int main(int argc, char **argv)
+{
+  if(argc == 1)
+  {
+    Person *persons_array = (Person*)malloc(sizeof(Person));
+    (persons_array)->gender_ = 3;
+    waitForInput(persons_array);
+  }
+  else if(argc == 2)
+  {
+    char *file_name = argv[1];
+    if(!fileExists(file_name))
+    {
+      printf("[ERR] Could not read file.\n");
+      return ERROR_FILE_COULD_NOT_BE_READ;
+    }
+    waitForInput(parseDotFile(storeFileIntoMemory(file_name))); //NOTE: ovo mozda ljepse napisati
+  }
+  else
+  {
+    printf("Usage: ./ass [file-name]\\n\n");
+    return ERROR_TO_MANY_ARGUMENTS;
+  }
+  return 0;
+}
+
+//NOTE: main() je gotovo 
+//NOTE: fileIsWritable() je gotov
+//NOTE: fileExists() je gotov
+//NOTE: findPerson() je gotov
+//NOTE: storeFileIntoMemory() je gotov
+//NOTE: createPersonInstance() je gotov
+
+/**
+ * [parseDotFile description]
+ * @param file_content [description]
+ */
+
+Person *parseDotFile(char *file_content)
+{
+  int number_of_lines = 0;
+  int counter = 0;
+  while(*(file_content + counter) != '\0') // Counting number of lines
+  {
+    if(*(file_content + counter) == '\n')
+    {
+      number_of_lines++;
+    }
+    counter++;
+  }
+  char *lines_separated[number_of_lines];
+  int lines_separated_counter = 0;
+  int file_content_counter = 0;
+
+  counter = 0;
+  while(*(file_content + counter) != '\0') // Extrating values from single string to array of pointers to the specific adresses withing the string
+  {
+    if(*(file_content + counter) == '\n')
+    {
+      *(file_content + counter) = '\0'; //Puting null byte insted of newline so the string is later valid for string functions
+      lines_separated[lines_separated_counter] = file_content + file_content_counter;
+      
+      file_content_counter = counter + 1;
+      lines_separated_counter++;
+    }
+    ++counter;
+  }
+  // Provjeravamo da li je pocetak i da li zagrade odgovaraju
+  if(strcmp(lines_separated[0],"digraph FamilyTree") != 0 || strcmp(lines_separated[1],"{") != 0 || file_content[counter-1] != '}')
+  {
+    free(file_content);
+    printf("[ERR] Could not read file.\n");
+    exit(ERROR_FILE_COULD_NOT_BE_READ);
+  }
+  char name[256];
+
+  char gender[4];
+
+  BOOL gender_b;
+  int number_of_persons = 0;
+
+  Person *array_of_persons = (Person*)malloc(sizeof(Person)*lines_separated_counter); //This will be reallocated later
+
+  for(counter = 2; counter < lines_separated_counter; counter++ )
+  {
+    sscanf(lines_separated[counter]," \"%[^[][%[^]];",name,gender);
+    name[strlen(name)-1] = '\0'; // NOTE: Ovo se rješava zadnjeg praznog mjesta u stringu u imenu, provjeriti da li postoji bolje rješenje.
+    gender_b = (gender[0] == 'f') ? TRUE : FALSE;
+    if(findPerson(array_of_persons,number_of_persons,name,gender_b) == NULL)
+    {
+      Person *new_temp_person = createPersonInstance(name,gender_b,NULL,NULL);
+      strcpy(array_of_persons[number_of_persons].name_,new_temp_person->name_);
+      array_of_persons[number_of_persons].gender_ = new_temp_person->gender_;
+      array_of_persons[number_of_persons].mother_ = new_temp_person->mother_;
+      array_of_persons[number_of_persons].father_ = new_temp_person->father_;
+      free(new_temp_person);
+      number_of_persons++;
+    }
+  }
+  array_of_persons[number_of_persons].gender_ = 3; // This is like \0 in string so we know where our array ends
+  array_of_persons = (Person*)realloc(array_of_persons,sizeof(Person)*(number_of_persons+1));
+
+  /*
+  char name2[256];
+  char gender2[4];
+  sscanf(lines_separated[3]," \"%[^ [] [%[^]]%*[^->]->%*[^\"]\"%[^[] [%[^]]%*[^;]",name,gender,name2,gender2); //TODO: moraćemo čitati svake navodnike i onaj znak -> i ; na kraju da možemo prebrojati imaju li svi da vidimo da li je fajl važeći
+  printf("%s - %s - %s %s\n",name, gender,name2,gender2 );
+  printf("%d\n", sscanf(lines_separated[3]," \"%[^ [] [%[^]]%*[^->]->%*[^\"]\"%[^[] [%[^]]%*[^;]",name,gender,name2,gender2));
+  
+*/
+  listPersons(array_of_persons);
+  printf("File parsing successful...\n");
+  free(file_content);
+  return array_of_persons;
+}
+
 /**
  * [isValidName description]
  * @param  name [description]
@@ -60,8 +203,9 @@ void parseInput(char *input_command, Person *persons_array)
   }
   if(strcmp(input_command,"list") == 0)
   {
-      printf("Skoro pa kul\n"); //TODO : napraviti funkciju
+  	listPersons(persons_array);
   }
+
 }
 /**
  * [waitForInput description]
@@ -214,121 +358,3 @@ Person *findPerson(Person *persons, int number_of_persons, char *name, BOOL gend
   }
   return NULL;
 }
-/**
- * [parseDotFile description]
- * @param file_content [description]
- */
-Person *parseDotFile(char *file_content)
-{
-  int number_of_lines = 0;
-  int counter = 0;
-  while(*(file_content + counter) != '\0') // Counting number of lines
-  {
-    if(*(file_content + counter) == '\n')
-    {
-      number_of_lines++;
-    }
-    counter++;
-  }
-  char *lines_separated[number_of_lines];
-  int lines_separated_counter = 0;
-  int file_content_counter = 0;
-
-  counter = 0;
-  while(*(file_content + counter) != '\0') // Extrating values from single string to array of pointers to the specific adresses withing the string
-  {
-    if(*(file_content + counter) == '\n')
-    {
-      *(file_content + counter) = '\0'; //Puting null byte insted of newline so the string is later valid for string functions
-      lines_separated[lines_separated_counter] = file_content + file_content_counter;
-      
-      file_content_counter = counter + 1;
-      lines_separated_counter++;
-    }
-    ++counter;
-  }
-  // Provjeravamo da li je pocetak i da li zagrade odgovaraju
-  if(strcmp(lines_separated[0],"digraph FamilyTree") != 0 || strcmp(lines_separated[1],"{") != 0 || file_content[counter-1] != '}')
-  {
-    free(file_content);
-    printf("[ERR] Could not read file.\n");
-    exit(ERROR_FILE_COULD_NOT_BE_READ);
-  }
-  char name[256];
-  char gender[4];
-  BOOL gender_b;
-  int number_of_persons = 0;
-
-  Person *array_of_persons = (Person*)malloc(sizeof(Person)*lines_separated_counter); //This will be reallocated later
-
-  for(counter = 2; counter < lines_separated_counter; counter++ )
-  {
-    sscanf(lines_separated[counter]," \"%[^[][%[^]];",name,gender);
-    name[strlen(name)-1] = '\0'; // NOTE: Ovo se rješava zadnjeg praznog mjesta u stringu u imenu, provjeriti da li postoji bolje rješenje.
-    gender_b = (gender[0] == 'f') ? TRUE : FALSE;
-    if(findPerson(array_of_persons,number_of_persons,name,gender_b) == NULL)
-    {
-      Person *new_temp_person = createPersonInstance(name,gender_b,NULL,NULL);
-      strcpy(array_of_persons[number_of_persons].name_,new_temp_person->name_);
-      array_of_persons[number_of_persons].gender_ = new_temp_person->gender_;
-      array_of_persons[number_of_persons].mother_ = new_temp_person->mother_;
-      array_of_persons[number_of_persons].father_ = new_temp_person->father_;
-      free(new_temp_person);
-      number_of_persons++;
-    }
-  }
-  array_of_persons[number_of_persons].gender_ = 3; // This is like \0 in string so we know where our array ends
-  array_of_persons = (Person*)realloc(array_of_persons,sizeof(Person)*(number_of_persons+1));
-
-  /*
-  char name2[256];
-  char gender2[4];
-  sscanf(lines_separated[3]," \"%[^ [] [%[^]]%*[^->]->%*[^\"]\"%[^[] [%[^]]%*[^;]",name,gender,name2,gender2); //TODO: moraćemo čitati svake navodnike i onaj znak -> i ; na kraju da možemo prebrojati imaju li svi da vidimo da li je fajl važeći
-  printf("%s - %s - %s %s\n",name, gender,name2,gender2 );
-  printf("%d\n", sscanf(lines_separated[3]," \"%[^ [] [%[^]]%*[^->]->%*[^\"]\"%[^[] [%[^]]%*[^;]",name,gender,name2,gender2));
-  
-*/
-  listPersons(array_of_persons);
-  printf("File parsing successful...\n");
-  free(file_content);
-  return array_of_persons;
-}
-/**
- * [main description]
- * @param  argc [description]
- * @param  argv [description]
- * @return      [description]
- */
-int main(int argc, char **argv)
-{
-  if(argc == 1)
-  {
-    printf("%d\n", isValidName("Marija"));
-    Person *persons_array = (Person*)malloc(sizeof(Person));
-    (persons_array)->gender_ = 3;
-    waitForInput(persons_array);
-  }
-  else if(argc == 2)
-  {
-    char *file_name = argv[1];
-    if(!fileExists(file_name))
-    {
-      printf("[ERR] Could not read file.\n");
-      return ERROR_FILE_COULD_NOT_BE_READ;
-    }
-    waitForInput(parseDotFile(storeFileIntoMemory(file_name))); //NOTE: ovo mozda ljepse napisati
-  }
-  else
-  {
-    printf("Usage: ./ass [file-name]\\n\n");
-    return ERROR_TO_MANY_ARGUMENTS;
-  }
-  return 0;
-}
-
-//NOTE: main() je gotovo 
-//NOTE: fileIsWritable() je gotov
-//NOTE: fileExists() je gotov
-//NOTE: findPerson() je gotov
-//NOTE: storeFileIntoMemory() je gotov
-//NOTE: createPersonInstance() je gotov
